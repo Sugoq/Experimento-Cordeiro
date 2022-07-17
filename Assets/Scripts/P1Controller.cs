ad4ad747e0d49a248892ba2d52f0ec254e499447
Sugoq <pedrohcherubini@gmail.com>
2022-07-17 15:15:39 -0300

    Changing P1 Script

--- Assets/Scripts/P1Controller.cs
+++ Assets/Scripts/P1Controller.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Player2Controller : MonoBehaviour
+public class P1Controller : MonoBehaviour
 {
     [SerializeField] bool isOnGround;
     public float speed;
--- Assets/Scripts/P2Controller.cs
+++ Assets/Scripts/P2Controller.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour
+public class P2Controller : MonoBehaviour
 {
     public float speed;
     public bool isPlayer1Turn;
