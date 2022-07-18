5093254a6c0bca18b3402da4b4fa98e8c30d25ae
Sugoq <pedrohcherubini@gmail.com>
2022-07-18 00:37:02 -0300

    Adding max switch times

--- Assets/Scripts/SwitchCharacter.cs
+++ Assets/Scripts/SwitchCharacter.cs
@@ -8,6 +8,8 @@ public class SwitchCharacter : MonoBehaviour
     
     public bool isP1Turn;
     public GameObject p1, p2;
+    public int maxSwitchTimes;
+    int switchTimes;
 
     private void Awake()
     {
@@ -22,17 +24,23 @@ public class SwitchCharacter : MonoBehaviour
 
     public void Switch()
     {
+        if (switchTimes == maxSwitchTimes) return;
+        
         if (isP1Turn)
         {
             p1.GetComponent<P1Controller>().enabled = false;
             p2.GetComponent<P2Controller>().enabled = true;
             isP1Turn = false;
+            switchTimes++;
+            
         }
         else
         {
             p1.GetComponent<P1Controller>().enabled = true;
             p2.GetComponent<P2Controller>().enabled = false;
             isP1Turn = true;
+            switchTimes++;
+
         }
     }
 }
